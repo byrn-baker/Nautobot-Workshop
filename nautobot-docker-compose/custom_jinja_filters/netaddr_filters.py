f867120f2ac73408cd4a214d0a80b7242a99504d
byrn-baker <byrnbaker@icloud.com>
2025-05-28 02:36:34 +0000

    updated templates and fixed the ansible playbook to flatten the nautobot query results under each device

--- nautobot-docker-compose/custom_jinja_filters/netaddr_filters.py
+++ nautobot-docker-compose/custom_jinja_filters/netaddr_filters.py
@@ -1,12 +1,25 @@
 from netaddr import IPNetwork
 from django_jinja import library
 
-
 @library.filter
-def ipv4_address(value):
-    return str(IPNetwork(value).ip)
+def ipaddr(value, operation=None):
+    """Mimic Ansible's ipaddr filter."""
+    try:
+        ip = IPNetwork(value)
+    except Exception:
+        return value  # Fail gracefully if it's not CIDR
 
-
-@library.filter
-def netmask(value):
-    return str(IPNetwork(value).netmask)
+    if operation == "address":
+        return str(ip.ip)
+    elif operation == "netmask":
+        return str(ip.netmask)
+    elif operation == "prefix":
+        return str(ip.prefixlen)
+    elif operation == "network":
+        return str(ip.network)
+    elif operation == "broadcast":
+        return str(ip.broadcast)
+    elif operation == "hostmask":
+        return str(ip.hostmask)
+    else:
+        return str(ip)  # fallback
